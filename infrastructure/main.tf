1cb8f2977ca3cd40c801eb5615898e84fc300d81
Abdul <abdul@intodepth.in>
2025-03-04 17:49:46 +0530

    Subscription added in backend

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -7,6 +7,7 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
+    subscription_id      = "82e6f7b9-feb5-4db0-9115-0528fa4ad1c7"
     resource_group_name = "generalRG"
     storage_account_name = "abdbackends"
     container_name = "bw89state"
